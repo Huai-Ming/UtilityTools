d1ff6ed6eea39af06beca0f96310745117488159
Huai-Ming <1604723751@qq.com>
2016-08-07 12:27:39 +0800

    Add HttpRequest utility to solution

---
+++ UtilityTools/HttpUtility/Basic/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HttpUtility.Basic
+{
+    public class Client
+    {
+        private static void Main(string[] args)
+        {
+            string reponse = HttpRequestHelper.GetValueFromWebApiByGet("http://www.baidu.com");
+            Console.WriteLine((reponse));
+
+            Console.WriteLine("Press any key to close the application");
+            Console.ReadKey();
+        }
+    }
+}
